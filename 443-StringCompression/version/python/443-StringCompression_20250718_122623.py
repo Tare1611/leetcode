13906bc30e60396433c15b27c561e934dd5bf622
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-18 12:26:23 -0500

    Create 443-StringCompression

---
+++ 443-StringCompression/version/python/443-StringCompression_20250718_122623.py
@@ -0,0 +1,19 @@
+# Last updated: 7/18/2025, 12:26:23 PM
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        #  Approach 2 pointers requires sorting, and a removal flag
+        # TC - O(nlogn) SC - O(1)
+        nums.sort()
+        l = 0
+        r = len(nums) - 1
+        check = 0
+        while l < r:
+            if nums[l] + nums[r] == k:
+                check += 1
+                l += 1
+                r -= 1
+            elif nums[l] + nums[r] < k:
+                l += 1
+            else: 
+                r -= 1
+        return check
