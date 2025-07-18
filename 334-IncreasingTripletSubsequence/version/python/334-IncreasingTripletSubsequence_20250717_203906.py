1cb5c1677d07a5825713eafe64bd15b4b612ecd3
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-17 20:39:07 -0500

    Create 334-IncreasingTripletSubsequence

---
+++ 334-IncreasingTripletSubsequence/version/python/334-IncreasingTripletSubsequence_20250717_203906.py
@@ -0,0 +1,28 @@
+# Last updated: 7/17/2025, 8:39:06 PM
+# This is partial my approach and partial online.
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        # Approach - Use for loop to traverse through
+        # TC - O(n) SC - O(1)
+        result = []
+        n = len(chars)
+        num = 1
+
+        if n <= 1:
+            return n
+
+        for i in range(1, n):
+            if chars[i - 1] == chars[i]:
+                num += 1
+            else:
+                result.append(chars[i - 1])
+                if num > 1:
+                    result.extend(list(str(num)))
+                num = 1
+
+        result.append(chars[-1])
+        if num > 1:
+            result.extend(list(str(num)))
+
+        chars[:] = result
+        return len(chars)
