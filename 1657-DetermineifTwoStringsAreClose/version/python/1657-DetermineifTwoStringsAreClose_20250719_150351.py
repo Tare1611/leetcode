f5b27b0ceb554362834106d6391f340b8bc27b8c
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-19 15:03:51 -0500

    Create 1657-DetermineifTwoStringsAreClose

---
+++ 1657-DetermineifTwoStringsAreClose/version/python/1657-DetermineifTwoStringsAreClose_20250719_150351.py
@@ -0,0 +1,25 @@
+# Last updated: 7/19/2025, 3:03:51 PM
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        # Approach - Maintain 2 queues for senate and check the index and then pop
+        # TC - O(n) SC - O(n)
+        from collections import deque
+        n = len(senate)
+        r = deque()
+        d = deque()
+
+        for i, ch in enumerate(senate):
+            if ch == 'R':
+                r.append(i)
+            else:
+                d. append(i)
+        
+        while r and d:
+            radiant = r.popleft()
+            dire = d.popleft()
+            if radiant < dire:
+                r.append(radiant + n)
+            else:
+                d.append(dire + n)
+
+        return "Radiant" if r else "Dire"
