f95d3acf0a158bbaf6092c07f03ea7bb225936ae
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-19 13:11:34 -0500

    Create 1657-DetermineifTwoStringsAreClose

---
+++ 1657-DetermineifTwoStringsAreClose/version/python/1657-DetermineifTwoStringsAreClose_20250719_131134.py
@@ -0,0 +1,29 @@
+# Last updated: 7/19/2025, 1:11:34 PM
+# Optimal Approach
+class Solution:
+    def removeStars(self, s: str) -> str:
+        # Brute force approach use stack to store values and pop when *. 
+        # TC - O(n^2) SC - O(n)
+        # string_stack = []
+
+        # for ch in s:
+        #     if ch == '*':
+        #         string_stack.pop()
+        #     else:
+        #         string_stack.append(ch)
+        # res_string = ""
+        # for i in range(len(string_stack)):
+        #     res_string = res_string + string_stack[i]
+        # return res_string
+
+        # Optimal Approach - Use inplace join instead of sting concatenation
+        # TC - O(n) SC - O(n)
+
+        stack = []
+        for ch in s:
+            if ch == "*":
+                stack.pop()
+            else:
+                stack.append(ch)
+        
+        return ''.join(stack)
