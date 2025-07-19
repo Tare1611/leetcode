e026129d34320fb5e2e436c2493e0c04082438df
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-19 15:55:27 -0500

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.py
@@ -0,0 +1,20 @@
+# Last updated: 7/19/2025, 3:55:27 PM
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        # Approach - Use a dictionary to store key-value pair and then use the len function to check.
+        # TC - O(n) SC - O(n)
+        # freq = {}
+        # for item in arr:
+        #     freq[item] = freq.get(item, 0) + 1
+        # print(freq)
+        # res1 =set(freq.values())
+        # print(res1)
+        # if len(res1) == len(freq):
+        #     return True
+        # return False
+
+        # Cleaner Approach - Use counter to create the freq map and then again len to check
+        from collections import Counter
+        freq = Counter(arr)
+
+        return len(set(freq.values())) == len(freq)
