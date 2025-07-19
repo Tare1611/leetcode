b6814237fa7b129169f98b58ab8106cee27ee4f5
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-19 15:55:11 -0500

    Create 1657-DetermineifTwoStringsAreClose

---
+++ 1657-DetermineifTwoStringsAreClose/version/python/1657-DetermineifTwoStringsAreClose_20250719_155511.py
@@ -0,0 +1,43 @@
+# Last updated: 7/19/2025, 3:55:11 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Brute force approach - loop through list to find the length, then calc middle and create a dummy list for removal
+        # TC - O(n) SC - O(1) 2 loops but not nested
+        length = 0
+        if not head or not head.next:
+            return None
+        
+        curr = head
+        while curr:
+            length += 1
+            curr = curr.next
+
+        mid = length // 2
+        curr = head
+        for _ in range(mid - 1):
+            curr = curr.next
+        curr.next = curr.next.next
+
+        return head
+
+        # Optimal Approach - Use two pointers fast and slow
+        # TC - O(n) SC - O(1) Single loop
+
+        if not head or not head.next:
+            return None
+        
+        fast = slow = head
+        prev = None
+
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = slow.next
+
+        return head
