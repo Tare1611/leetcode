a94f256a374e9f6eec48a0bc74665cb72a728a01
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-22 11:35:04 -0500

    Create 1161-MaximumLevelSumofaBinaryTree

---
+++ 1161-MaximumLevelSumofaBinaryTree/version/python/1161-MaximumLevelSumofaBinaryTree_20250722_113504.py
@@ -0,0 +1,41 @@
+# Last updated: 7/22/2025, 11:35:04 AM
+# Approach is to use deque and bfs to traverse the tree level wise
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        #  Approach - Traverse tree level by level using bfs. Use deque for help.
+        # TC - O(n) SC - O(w) width of tree
+        from collections import deque
+        if not root:
+            return 0
+        
+        level = 1
+        max_level = 1
+        max_sum = float('-inf')
+
+        q = deque([root])
+
+        while q:
+            level_size = len(q)
+            current_sum = 0
+
+            for _ in range(level_size):
+                node = q.popleft()
+                current_sum += node.val
+            
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            if current_sum > max_sum:
+                max_sum = current_sum
+                max_level = level
+            
+            level += 1
+        return max_level
