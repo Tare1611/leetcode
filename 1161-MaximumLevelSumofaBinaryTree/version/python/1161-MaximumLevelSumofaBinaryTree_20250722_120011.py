5ecb8b836a70fed9298c8131330c4f378cda76f8
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-22 12:00:12 -0500

    Create 1161-MaximumLevelSumofaBinaryTree

---
+++ 1161-MaximumLevelSumofaBinaryTree/version/python/1161-MaximumLevelSumofaBinaryTree_20250722_120011.py
@@ -0,0 +1,23 @@
+# Last updated: 7/22/2025, 12:00:11 PM
+# Recursive approach
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # Recursive Approach - Using a function to handle the calculation
+        # TC - O(n) SC - O(h) - due to call stack
+        def dfs(node: Optional[TreeNode], val):
+            if not node:
+                return None
+            if node.val == val:
+                return node
+            elif node.val < val:
+                return dfs(node.right, val)
+            elif node.val > val:
+                return dfs(node.left, val)
+        
+        return dfs(root, val)
