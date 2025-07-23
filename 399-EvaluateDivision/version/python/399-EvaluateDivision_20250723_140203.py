2f6c5fda8560a07290c161d4d3c39c2492116024
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-23 14:02:04 -0500

    Create 399-EvaluateDivision

---
+++ 399-EvaluateDivision/version/python/399-EvaluateDivision_20250723_140203.py
@@ -0,0 +1,27 @@
+# Last updated: 7/23/2025, 2:02:03 PM
+# Approach is to use bfs and move in different directions and mark them with "+" for visited
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        # Approach - USe bruteforce to traverse all the nearest locations. Use set or mark the visited location. If answer return count else  -1
+        # TC - (m*n) SC - O(m*n)
+        from collections import deque
+        rows = len(maze)
+        cols = len(maze[0])
+        q = deque()
+        q.append((entrance[0], entrance[1], 0))
+        
+        directions = [(-1,0), (0, -1), (1, 0), (0, 1)]
+
+        while q:
+            r, c, steps = q.popleft()
+
+            if ( r != entrance[0] or c != entrance[1]) and ( r == 0 or c == 0 or r == rows - 1 or c == cols - 1):
+                return steps
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if 0 <= nr < rows and  0 <= nc < cols and maze[nr][nc] == ".":
+                    q.append((nr, nc, steps + 1))
+                    maze[nr][nc] = "+" # Mark visited 
+        return -1
