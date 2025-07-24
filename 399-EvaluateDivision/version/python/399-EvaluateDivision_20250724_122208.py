8ad20457b548f980dfab09e56611868befe8d61b
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-24 12:22:08 -0500

    Create 399-EvaluateDivision

---
+++ 399-EvaluateDivision/version/python/399-EvaluateDivision_20250724_122208.py
@@ -0,0 +1,24 @@
+# Last updated: 7/24/2025, 12:22:08 PM
+# Use zip to generate pairs from n1 and n2, then sort the n2 in descending. Ine minheap push elements and keep a check on len == k
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        # Brute Force Approach - Try all combinations of length k, and then compute scores accordingly
+        # TC - O()
+        pairs = [(n1,n2) for n1,n2 in zip(nums1, nums2)]
+        pairs = sorted(pairs, key = lambda p: p[1], reverse=True)
+
+        minHeap = []
+        n1Sum = 0
+        res = 0
+
+        for n1, n2 in pairs:
+            n1Sum += n1
+            heapq.heappush(minHeap, n1)
+
+            if len(minHeap) > k:
+                n1Pop = heapq.heappop(minHeap)
+                n1Sum -= n1Pop
+            
+            if len(minHeap) == k:
+                res = max(res, n1Sum * n2)
+        return res
