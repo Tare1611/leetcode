734f72c510e53415fe45199a2c924fc557f4ef21
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-25 14:35:54 -0500

    Create 216-CombinationSumIII

---
+++ 216-CombinationSumIII/version/python/216-CombinationSumIII_20250725_143554.py
@@ -0,0 +1,16 @@
+# Last updated: 7/25/2025, 2:35:54 PM
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Approach - Assign the number of moves available to a matrix on all cell blocks
+        # For start the last row and last column apart from finish will have 1, then start backtracking while adding 
+
+        # TC - O(m*n) SC - O(n)
+
+        row = [1]*n
+        
+        for i in range(m-1):
+            newRow = [1]*n
+            for j in range(n-2, -1, -1):
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow
+        return row[0]
