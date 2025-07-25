1f2f255b80c8092f7711c0594b6e550b20a75803
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-25 17:33:37 -0500

    Create 216-CombinationSumIII

---
+++ 216-CombinationSumIII/version/python/216-CombinationSumIII_20250725_173337.py
@@ -0,0 +1,47 @@
+# Last updated: 7/25/2025, 5:33:37 PM
+# Implement tree and set the last char of word as end of word
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr  = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+# Approach - Have a tree structure and keep adding character to the tree. Mark the last character for a given node as end to id.
+# TC - O(n), SC - O(n)
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
