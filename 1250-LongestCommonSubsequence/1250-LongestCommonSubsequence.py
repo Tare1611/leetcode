a4c7b391dc6ad324c40c7135e0fa887fe93b8c11
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-26 14:32:14 -0500

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.py
@@ -0,0 +1,15 @@
+# Last updated: 7/26/2025, 2:32:12 PM
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Approach - MAintain a 2-D array and store the values of common in substring while checking if first ch is same.
+        # Bottom Up approach
+        # TC - O(n*m) SC - O(n*m)
+
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        for i in range(len(text1)-1, -1, -1):
+            for j in range(len(text2)-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        return dp[0][0] 
