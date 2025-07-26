4fc497e50b23c661f2d16f9e5c0e02f9e6117c00
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-26 14:32:21 -0500

    Create 806-DominoAndTrominoTiling

---
+++ 806-DominoAndTrominoTiling/806-DominoAndTrominoTiling.py
@@ -0,0 +1,30 @@
+# Last updated: 7/26/2025, 2:32:20 PM
+class Solution:
+    def numTilings(self, n: int) -> int:
+        # Recursive Approach with two function one for full cover and partial
+        # TC - O(n) SC - O(n)
+
+        MOD = 10**9 + 7
+
+        pCache = {}
+        nCache = {}
+
+        def partial(n):
+            if n == 2:
+                return 1
+            if n in pCache:
+                return pCache[n]
+            
+            pCache[n] = partial(n-1) + full(n-2)
+            return pCache[n]
+
+        def full(n):
+            if n <= 2:
+                return n
+            if n in nCache:
+                return nCache[n]
+            
+            nCache[n] = full(n-1) + full(n-2) + 2 * partial(n-1)
+            return nCache[n]
+
+        return full(n) % MOD
