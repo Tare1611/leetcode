79c520afb037593131d8eb72b9a95f01aa3b0f07
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-26 14:32:28 -0500

    Create 452-MinimumNumberOfArrowsToBurstBalloons

---
+++ 452-MinimumNumberOfArrowsToBurstBalloons/452-MinimumNumberOfArrowsToBurstBalloons.py
@@ -0,0 +1,18 @@
+# Last updated: 7/26/2025, 2:32:27 PM
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        # Approach - Consider the balloons as intervals, and find the max list of overlapping.
+        # TC - O(n)  SC - O(n)
+        points.sort()
+
+        result = len(points) # Max num of arrows if no overlap
+        prev = points[0]
+        for i in range(1, len(points)):
+            curr = points[i]
+
+            if curr[0] <= prev[1]:
+                result -= 1
+                prev = [curr[0], min(curr[1], prev[1])]
+            else:
+                prev = curr
+        return result
