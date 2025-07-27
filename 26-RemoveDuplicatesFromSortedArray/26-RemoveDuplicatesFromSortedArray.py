c789215ac05dc9883bd4ec90cb5bbf4e93b99e46
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-26 19:45:49 -0500

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,13 @@
+# Last updated: 7/26/2025, 7:45:49 PM
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Approach - Use two pointer setup. Start from index 1 and then move ahead when you find a new value.
+        # TC - O(n) SC - O(1)
+        left = 1
+        
+        for right in range(1, len(nums)):
+            if nums[right] != nums[right - 1]:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
