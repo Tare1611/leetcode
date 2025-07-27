f5da47730f9a05ac7dfa1ec78b2d559a8dbc5dec
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-26 20:43:48 -0500

    Create 27-RemoveElement

---
+++ 27-RemoveElement/version/python/27-RemoveElement_20250726_204348.py
@@ -0,0 +1,15 @@
+# Last updated: 7/26/2025, 8:43:48 PM
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # Approach - take a pointer to update the first element of the array and move forward
+        # TC - O(n) SC - O(1)
+        # prevlen = len(nums)
+        left = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[left] = nums[i]
+                left += 1
+        # print(nums)
+        return left
+        # return left - 1
