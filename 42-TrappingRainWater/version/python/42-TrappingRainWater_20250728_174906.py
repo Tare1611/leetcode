f8e6c4fc6756a8563e52139685f100dd0ca3a590
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 17:49:07 -0500

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/python/42-TrappingRainWater_20250728_174906.py
@@ -0,0 +1,29 @@
+# Last updated: 7/28/2025, 5:49:06 PM
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        # Approach - Use a deque and work with a loop to reach "k"
+        # TC - O(n*k)
+        # SC - O(n)
+        # q = deque()
+        
+        # for i in range( 1, n + 1):
+        #     q.append(i)
+        
+        # while len(q) > 1:
+        #     for i in range(k - 1):
+        #         num = q.popleft()
+        #         q.append(num)
+        #     q.popleft()
+        
+        # return q[0]
+
+        # Optimal Approach - Use recursion to reduce the running time and space used.
+        # TC - O(n)
+        # SC - O(n) Due to recursion stack
+
+        def helper(n, k):
+            if n == 1:
+                return 0
+            return(helper(n-1, k) + k) % n
+        return helper(n, k) + 1
+
