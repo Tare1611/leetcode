3eabf2aa9294485a6896462e6f3b98ec74522c9e
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 18:25:52 -0500

    Create 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold

---
+++ 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/version/python/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold_20250728_182552.py
@@ -0,0 +1,17 @@
+# Last updated: 7/28/2025, 6:25:52 PM
+# Sliding Window problem
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        # Approach - Implement a Sliding window and keep updating the outgoing and incoming values and calculate the avg
+        # TC - O(n)
+        # SC - O(1)
+
+        result = 0
+        currSum = sum(arr[:k-1])
+
+        for left in range(len(arr) - k + 1):
+            currSum += arr[left + k - 1]
+            if  (currSum/k) >= threshold:
+                result += 1
+            currSum -= arr[left]
+        return result
