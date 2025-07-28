4e5e583d427360e429a320775105eed36a240908
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 09:51:23 -0500

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/version/python/150-EvaluateReversePolishNotation_20250728_095122.py
@@ -0,0 +1,37 @@
+# Last updated: 7/28/2025, 9:51:22 AM
+# Use stack to reduce the SC and TC
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Approach - Use a stack to store the digits and pop them to perform the calculation, append back the result
+        # TC - O(n) since using a single for loop
+        # SC - O(n) since using a stack to store the numbers
+        
+        res = 0
+        stack = []
+        for i in range(len(tokens)):
+            if tokens[i].isdigit():
+                stack.append(tokens[i])
+            elif tokens[i] == "+":
+                b = int(stack.pop())
+                a = int(stack.pop())
+                res = a + b
+                stack.append(res)
+            elif tokens[i] == "-":
+                b = int(stack.pop())
+                a = int(stack.pop())
+                res = a - b
+                stack.append(res)
+            elif tokens[i] == "*":
+                b = int(stack.pop())
+                a = int(stack.pop())
+                res = a * b
+                stack.append(res)
+            elif tokens[i] == "/":
+                b = int(stack.pop())
+                a = int(stack.pop())
+                res = a / b
+                stack.append(res)
+            else:
+                stack.append(tokens[i])
+
+        return int(stack[0])
