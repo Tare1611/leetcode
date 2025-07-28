56f082384035f5d9d8da3f904b5638928932d050
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 11:45:36 -0500

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/version/python/150-EvaluateReversePolishNotation_20250728_114535.py
@@ -0,0 +1,25 @@
+# Last updated: 7/28/2025, 11:45:35 AM
+# Implement a stack to track index and height
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # Approach - Have a stack and store the index and height
+        #  - keep track of max_area before popping the greater height if we encounter a smaller height
+        #  - update the index of the smaller to previous one
+        # TC - O()
+        # SC - O()
+
+        stack = [] #pair of elements [index, heights]
+        max_area = 0
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                max_area = max(max_area, height*(i - index))
+                start = index
+            stack.append([start,h])
+        
+        for i, h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
+
+        return max_area
