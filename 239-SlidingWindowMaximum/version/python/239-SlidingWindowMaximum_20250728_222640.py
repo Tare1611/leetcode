14058f24faed7a9bd32af7e76ba6d19db69dd92d
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 22:26:41 -0500

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/version/python/239-SlidingWindowMaximum_20250728_222640.py
@@ -0,0 +1,25 @@
+# Last updated: 7/28/2025, 10:26:40 PM
+# Use deque to store the values in window and update as we move ahead
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Approach - Use a deque to store the values in the window, check for the max value and then keep on updating the deque when you slide the window
+        # TC - O(n)
+        # SC - O(n)
+
+        q = collections.deque()
+        n = len(nums)
+        result = []
+        l = r = 0
+        while r < n:
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                result.append(nums[q[0]])
+                l += 1
+            r += 1 
+        return result
