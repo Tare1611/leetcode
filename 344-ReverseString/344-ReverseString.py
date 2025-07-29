7e253ff4d28300c635da67a4c147de90d0378486
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-29 13:46:25 -0500

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.py
@@ -0,0 +1,16 @@
+# Last updated: 7/29/2025, 1:46:24 PM
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Approach - Two pointer, replace the element
+        # TC - O(n)
+        # SC - O(1)
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
