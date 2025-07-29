4c565874b3a2e85fb5e345daca18a56b99fc9898
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-29 09:34:07 -0500

    Create 25-ReverseNodesink-Group

---
+++ 25-ReverseNodesink-Group/version/python/25-ReverseNodesink-Group_20250729_093406.py
@@ -0,0 +1,41 @@
+# Last updated: 7/29/2025, 9:34:06 AM
+# Keep checking for k elements and then group them for better working
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Approach - Use Dummy Node to traverse the linked list, store the value of group previous to curr
+        # TC - O(n)
+        # SC - O(1)
+
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            # reversing the group using 2 pointers
+            prev, curr = kth.next, groupPrev.next
+
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr        
