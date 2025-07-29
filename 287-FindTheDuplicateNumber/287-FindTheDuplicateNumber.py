0253a998510182578ad1f4d657a87d0d5936f7dc
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 23:02:30 -0500

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.py
@@ -0,0 +1,20 @@
+# Last updated: 7/28/2025, 11:02:30 PM
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Approach - Use Floyd's Tortoise Hare Algo
+        # TC - O(n) 
+        # SC - O(1)
+
+        slow = fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
