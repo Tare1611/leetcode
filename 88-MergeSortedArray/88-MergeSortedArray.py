e6e69d9e568814dd276766f72d7cfee6e40216be
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-28 23:02:46 -0500

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,21 @@
+# Last updated: 7/28/2025, 11:02:46 PM
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Approach - Use a pointer to start from the last and work way ahead till 0
+        # TC - O(n) SC - O(1) 
+        last = m + n - 1
+        while m > 0 and n > 0:
+            if nums1[m - 1 ] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
+        
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n, last = n - 1, last - 1
