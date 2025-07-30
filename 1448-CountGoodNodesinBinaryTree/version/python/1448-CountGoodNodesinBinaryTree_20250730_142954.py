136f68c793de6c6ba24ad366d8b7fafaf294b7fe
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-30 14:29:54 -0500

    Create 1448-CountGoodNodesinBinaryTree

---
+++ 1448-CountGoodNodesinBinaryTree/version/python/1448-CountGoodNodesinBinaryTree_20250730_142954.py
@@ -0,0 +1,25 @@
+# Last updated: 7/30/2025, 2:29:54 PM
+# recursively go through the buildTree function
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # Approach - Prorder always has the root first and then it's left and right child nodes. 
+        # - Inorder helps find the partition between left subtree and right subtree.
+        # TC - O(n^2)
+        # SC - O(n)
+
+        if not preorder or not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid+1:])
+        
+        return root
