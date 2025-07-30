f3773620ffbcdb2b0b56ff0b2df13bded24a2d2c
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-30 13:47:09 -0500

    Create 1448-CountGoodNodesinBinaryTree

---
+++ 1448-CountGoodNodesinBinaryTree/version/python/1448-CountGoodNodesinBinaryTree_20250730_134709.py
@@ -0,0 +1,25 @@
+# Last updated: 7/30/2025, 1:47:09 PM
+# Use DFS to traverse through the tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # Approach - Use dfs to check if all the nodes follow the condition or not.
+        # - Condition is left is smaller than root
+        # - Right is larger than the Root.
+        # - Check for this condition recursively.
+        # TC - O(n)
+        # SC - O(n) - For recursion stack, else for each iteration it would be O(1)
+
+        def dfs(node, left, right):
+            if not node:
+                return True
+            if not (node.val < right and node.val > left):
+                return False
+            
+            return (dfs(node.left, left, node.val) and dfs(node.right, node.val, right))
+        return dfs(root, float("-inf"), float("inf"))
