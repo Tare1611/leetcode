ed5c4c0015dd6957ff2f53aba5d4838f2ab80171
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-07-31 10:10:54 -0500

    Create 355-DesignTwitter

---
+++ 355-DesignTwitter/version/python/355-DesignTwitter_20250731_101053.py
@@ -0,0 +1,50 @@
+# Last updated: 7/31/2025, 10:10:53 AM
+# Use hashmap to store the value and return using minHeap
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.tweetMap = defaultdict(list) # userID -> list of [count, tweetIDS]
+        self.followMap = defaultdict(set) # userID -> set of followeeID
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetMap[userId].append([self.count, tweetId])
+        self.count -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        result = [] # list of tweets order starting from the recent
+        minHeap = [] # to find out the most recent one
+        
+        self.followMap[userId].add(userId)
+        
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                count, tweetId = self.tweetMap[followeeId][index]
+                minHeap.append([count, tweetId, followeeId, index - 1])
+        heapq.heapify(minHeap)
+        while minHeap and len(result) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            result.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+        return result
+        
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
+
+# Approach - Use Hashmap to store the values and retrieve, 
+# - Initialize defaultdict for storing and creating.
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
