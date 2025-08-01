ef05a3443ca98e74d20959af6e9156e98da76df5
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-01 18:01:39 -0500

    Create 778-ReorganizeString

---
+++ 778-ReorganizeString/778-ReorganizeString.py
@@ -0,0 +1,30 @@
+# Last updated: 8/1/2025, 6:01:38 PM
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # Approach - Use Max heap to store the count of each character and then generate the output
+        # TC - O(n log k)
+        # SC - O(n)
+
+        count = Counter(s)
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
+        heapq.heapify(maxHeap)
+
+        prev = None
+        result = ""
+
+        while maxHeap or prev:
+            # most freq, except prev
+            if prev and not maxHeap:
+                return ""
+            cnt, char = heapq.heappop(maxHeap)
+            result += char
+            cnt += 1
+            
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            
+            if cnt != 0:
+                prev = [cnt, char]
+        
+        return result
