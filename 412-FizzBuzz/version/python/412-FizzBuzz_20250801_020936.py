a8d6685256aae6af8f77a69d689fa536d47f043d
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-01 02:09:36 -0500

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/version/python/412-FizzBuzz_20250801_020936.py
@@ -0,0 +1,32 @@
+# Last updated: 8/1/2025, 2:09:36 AM
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        # fizz = "Fizz"
+        # buzz = "Buzz"
+        # result = [""]*n
+        # print(n)
+        # for i in range(n):
+        #     temp = i+1
+        #     if temp%3 == 0 and temp%5==0:
+        #         result[i] ="FizzBuzz"
+        #     elif temp%3 ==0:
+        #         result[i] = "Fizz"
+        #     elif temp%5 == 0:
+        #         result[i] = "Buzz"
+        #     else:
+        #         result[i] = str(temp)
+        # return result
+
+        result = []
+        print(n)
+        for i in range(n):
+            temp = i+1
+            if temp%3 == 0 and temp%5==0:
+                result.append("FizzBuzz")
+            elif temp%3 ==0:
+                result.append("Fizz")
+            elif temp%5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(temp))
+        return result
