5bad3ddee4b7f3bd0c0db5281eff402aa9a02958
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-04 17:14:33 -0500

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.py
@@ -0,0 +1,18 @@
+# Last updated: 8/4/2025, 5:14:32 PM
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # Approach - Use binary search with left and right pointers moving towards the target
+        # - Return the first instance left meets target, and right meets target
+        # - If not present return [-1, -1]
+
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            if (nums[left] == target) and (nums[right] == target):
+                return [left, right]
+            if nums[left] < target:
+                left += 1
+            if nums[right] > target:
+                right -= 1
+        return [-1,-1]
