fad087effa95e6938410c6dc36c75279da42dd3a
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-03 22:32:36 -0500

    Create 802-KThSmallestPrimeFraction

---
+++ 802-KThSmallestPrimeFraction/802-KThSmallestPrimeFraction.py
@@ -0,0 +1,22 @@
+# Last updated: 8/3/2025, 10:32:35 PM
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        # Approach - Generate an array of the fraction values for the input array
+        # - return the pair of indices that generate the kth smallest value
+        # 
+
+        result = []
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                result.append([arr[i],arr[j], arr[i]/arr[j]])
+        
+        minHeap = []
+        for i in range(len(result)):
+           heapq.heappush(minHeap, [result[i][2], result[i][0], result[i][1]])
+        
+        res = 0
+        while k > 0:
+            a,b,c = heapq.heappop(minHeap)
+            k -= 1
+
+        return [b,c]     
