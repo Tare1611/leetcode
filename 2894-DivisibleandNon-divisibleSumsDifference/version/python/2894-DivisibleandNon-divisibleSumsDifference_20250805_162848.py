995632269eb061035aef18afaf1777f9ff8a25a7
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-05 16:28:48 -0500

    Create 2894-DivisibleandNon-divisibleSumsDifference

---
+++ 2894-DivisibleandNon-divisibleSumsDifference/version/python/2894-DivisibleandNon-divisibleSumsDifference_20250805_162848.py
@@ -0,0 +1,13 @@
+# Last updated: 8/5/2025, 4:28:48 PM
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        # Approach - Loop through the array, and check if the element is divisible or not and update the result accordingly
+        # TC - O(n)
+        # SC - O(1)
+        result = 0
+        for i in range(n+1):
+            if i%m != 0:
+                result += i
+            else:
+                result -= i
+        return result
