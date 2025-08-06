f84c6c9c14b62b1cd3fb1d091261cb2f8bc0e6fc
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-05 20:02:17 -0500

    Create 3791-FruitsIntoBasketsIii

---
+++ 3791-FruitsIntoBasketsIii/3791-FruitsIntoBasketsIii.py
@@ -0,0 +1,32 @@
+# Last updated: 8/5/2025, 8:02:17 PM
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        # Approach - Loop through the fruits and baskets array and find the first greater than or equal basket
+        # - To store the fruit correctly, we could use deque to pop the elements from the left and then return the len
+        n = len(baskets)
+        m = int(math.sqrt(n))
+        section = (n + m - 1) // m
+        count = 0
+        maxV = [0] * section
+
+        for i in range(n):
+            maxV[i // m] = max(maxV[i // m], baskets[i])
+
+        for fruit in fruits:
+            unset = 1
+            for sec in range(section):
+                if maxV[sec] < fruit:
+                    continue
+                choose = 0
+                maxV[sec] = 0
+                for i in range(m):
+                    pos = sec * m + i
+                    if pos < n and baskets[pos] >= fruit and not choose:
+                        baskets[pos] = 0
+                        choose = 1
+                    if pos < n:
+                        maxV[sec] = max(maxV[sec], baskets[pos])
+                unset = 0
+                break
+            count += unset
+        return count
