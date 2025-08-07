69868bd6ebc8a2f3a8bc07d5d00c88cbb3612668
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-07 16:05:25 -0500

    Create 76-MinimumWindowSubstring

---
+++ 76-MinimumWindowSubstring/version/python/76-MinimumWindowSubstring_20250807_160524.py
@@ -0,0 +1,47 @@
+# Last updated: 8/7/2025, 4:05:24 PM
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Approach - Create a map for the substring, keep increasing the window size from 0 to n to check for the substr
+        # - Keep a track of the number of letters of the substr that appear
+        # - Window stores the frequency of how many characters we need
+        # - countT stores the frequency of required characters in a substr
+        # TC - O(n)
+        # SC - O(n)
+
+        if t == "":
+            return ""
+
+        countT = {}
+        window = {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have = 0
+        need = len(countT)
+
+        result = [-1, -1]
+        resultLen = float("inf")
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                # update result and result length   
+                if (r - l + 1) < resultLen:
+                    result = [l,r]
+                    resultLen = (r - l + 1)
+                # pop from the left of window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l +=1
+        
+        l, r = result
+
+        return s[l:r+1] if resultLen != float("inf") else ""
