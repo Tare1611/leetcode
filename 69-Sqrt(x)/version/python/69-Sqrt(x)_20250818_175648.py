ab110854a03e989aa531715f9f751cf7675e8687
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2025-08-18 17:56:48 -0500

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/version/python/69-Sqrt(x)_20250818_175648.py
@@ -0,0 +1,23 @@
+# Last updated: 8/18/2025, 5:56:48 PM
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Brute Force - Use while loop and just go through the integers and find the closest i for square
+        # TC - O(n) SC - O(1)
+        # i = 0
+        # while i*i <= x:
+        #     i += 1
+        # return i - 1
+
+        # Optimal Approach - Use binary search divide the search area in half and test the midpoint.
+        # TC - O(log x) SC - O(1)
+        left = 0
+        right = x
+        result = 0
+        while left <= right:
+            mid = (left + right)//2
+            if mid * mid <= x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
