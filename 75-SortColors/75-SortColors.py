23832b5a766938de0ffb057cd9af2cc903ca6fc3
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2026-02-05 21:07:15 -0600

    File updated at 2/5/2026, 9:07:15 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,35 +1,17 @@
-# Last updated: 2/5/2026, 8:47:52 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-8        # Approach - Start with 2 variables Units and Digits
-9        # Keep a track of the linked lists and then if one ends append the other
-10        # Have a final result linked list as well.
-11        # TC -
-12        # SC - 
-13        digit = 0
-14        carry = 0
-15
-16        dummy_list = ListNode()
-17        curr = dummy_list
-18
-19        while l1 or l2 or carry:
-20            a = l1.val if l1 else 0
-21            b = l2.val if l2 else 0
-22            add = a + b + carry
-23            carry = add // 10
-24            unit = add % 10
-25
-26            curr.next = ListNode(unit)
-27            curr = curr.next
-28            l1 = l1.next if l1 else None
-29            l2 = l2.next if l2 else None
-30        
-31        curr.next = l1.next if l1 else None
-32        curr.next = l2.next if l2 else None
-33        return dummy_list.next
-34
+# Last updated: 2/5/2026, 9:07:15 PM
+1class Solution:
+2    def removeDuplicates(self, nums: List[int]) -> int:
+3        # Approach - Have a 2 pointer setup, where we start with the 1 index
+4        # We travel forward considering 0th index is unique and checking with prev
+5        # Second pointer is to update the array and keep a track on last
+6        # Unique value
+7        # TC
+8        # SC
+9
+10        j = 1
+11        
+12        for i in range(1, len(nums)):
+13            if nums[i] != nums[i-1]:
+14                nums[j] = nums[i]
+15                j += 1
+16        return j
