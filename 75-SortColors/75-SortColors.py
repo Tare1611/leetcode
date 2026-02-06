ffa059c8d89476eb87482416c577a88855967783
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2026-02-05 21:08:07 -0600

    File updated at 2/5/2026, 9:08:07 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,12 +1,12 @@
-# Last updated: 2/5/2026, 9:07:15 PM
+# Last updated: 2/5/2026, 9:08:07 PM
 1class Solution:
 2    def removeDuplicates(self, nums: List[int]) -> int:
 3        # Approach - Have a 2 pointer setup, where we start with the 1 index
 4        # We travel forward considering 0th index is unique and checking with prev
 5        # Second pointer is to update the array and keep a track on last
 6        # Unique value
-7        # TC
-8        # SC
+7        # TC - O(n)
+8        # SC - O(1)
 9
 10        j = 1
 11        
