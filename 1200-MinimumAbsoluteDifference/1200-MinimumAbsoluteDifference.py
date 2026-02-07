e86b20f2c02e5e8e7435f9b95be4f3976d19553f
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2026-02-06 21:58:37 -0600

    File updated at 2/6/2026, 9:58:37 PM

--- 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.py
+++ 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.py
@@ -1,18 +1,20 @@
-# Last updated: 2/6/2026, 9:57:55 PM
+# Last updated: 2/6/2026, 9:58:37 PM
 1class Solution:
 2    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-3        # Approach - Sort the array 
-4        arr.sort()
-5        # print(arr)
-6        result = []
-7        min_diff = float('inf')
-8
-9        for i in range(1, len(arr)):
-10            diff = arr[i] - arr[i-1]
-11            if diff < min_diff:
-12                res = [[arr[i-1], arr[i]]]
-13                min_diff = diff
-14            elif diff == min_diff:
-15                res.append([arr[i-1], arr[i]])
-16        return res
-17
+3        # Approach - Sort the array and then keep track of the difference and update min diff
+4        # TC - O(nlogn)
+5        # SC - O(n)
+6        arr.sort()
+7        # print(arr)
+8        result = []
+9        min_diff = float('inf')
+10
+11        for i in range(1, len(arr)):
+12            diff = arr[i] - arr[i-1]
+13            if diff < min_diff:
+14                res = [[arr[i-1], arr[i]]]
+15                min_diff = diff
+16            elif diff == min_diff:
+17                res.append([arr[i-1], arr[i]])
+18        return res
+19
