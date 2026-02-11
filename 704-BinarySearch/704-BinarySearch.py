1b9e400d5183a19dc61cabe4644e522ef66d8cda
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2026-02-10 20:01:10 -0600

    File updated at 2/10/2026, 8:01:10 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,21 +1,18 @@
-# Last updated: 2/10/2026, 7:53:54 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        def merge(t1, t2):
-            if t1 is None:
-                return t2
-            if t2 is None:
-                return t1
-
-            return TreeNode(
-                t1.val + t2.val,
-                merge(t1.left, t2.left),
-                merge(t1.right, t2.right)
-            )
-        return merge(root1, root2)
+# Last updated: 2/10/2026, 8:01:10 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.next = None
+6
+7class Solution:
+8    def hasCycle(self, head: Optional[ListNode]) -> bool:
+9        
+10        slow = fast = head
+11
+12        while fast and fast.next:
+13            slow = slow.next
+14            fast = fast.next.next
+15            if slow == fast:
+16                return True
+17        return False
